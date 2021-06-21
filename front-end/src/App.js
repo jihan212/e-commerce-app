b95f597216213b571f4836a20c8d1987ff5ea73f
snapper <zihanjashim2121@gmail.comm>
2021-06-21 22:52:50 +0600

    React set up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-     <h1>Heloo</h1>
+    <Header></Header>
+    <main>
+      <Container>
+        <h1>Heloo</h1>
+      </Container>
+    </main>
+    <Footer></Footer>
     </>
   );
 }
