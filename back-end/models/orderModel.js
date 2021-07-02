fa0dd37037a013b984142c1b452ae2b51b0d8d93
snapper <zihanjashim2121@gmail.comm>
2021-07-02 20:55:14 +0600

    Data Modeled

--- back-end/models/orderModel.js
+++ back-end/models/orderModel.js
@@ -7,34 +7,34 @@ const orderSchema = mongoose.Schema(
       required: true,
       ref: 'User',
     },
-        orderItems: [
-            {
-                name: { type: String, required: true },
-                qty: { type: Number, required: true },
-                image: { type: String, required: true },
-                price: { type: Number, required: true },
-                product: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    required: true,
-                    ref: 'Product',
-                },
-            }
-        ]
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+      },
+    ],
     shippingAddress: {
-        address: { type: String, required: true },
-        city: { type: String, required: true },
-        postalCode: { type: String, required: true },
-        country: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
     },
     paymentMethod: {
       type: String,
       required: true,
     },
     paymentResult: {
-        id: { type: String },
-        status: { type: String },
-        update_time: { type: String },
-        email_address: { type: String },
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
     },
     taxPrice: {
       type: Number,
@@ -55,18 +55,18 @@ const orderSchema = mongoose.Schema(
       type: Boolean,
       required: true,
       default: false,
-        },
+    },
     paidAt: {
       type: Date,
-        },
+    },
     isDelivered: {
       type: Boolean,
       required: true,
       default: false,
-        },
+    },
     deliveredAt: {
       type: Date,
-        }
+    },
   },
   {
     timestamps: true,
