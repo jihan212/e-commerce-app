49a86ee43458b1ec8fde1b75f9481ef81d657ccf
snapper <zihanjashim2121@gmail.comm>
2021-08-11 00:32:08 +0600

    User Register Reducer, Action & Screen added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './screen/HomeScreen'
 import ProductScreen from './screen/ProductScreen'
 import CartScreen from './screen/CartScreen'
 import LoginScreen from './screen/LoginScreen'
+import RegisterScreen from './screen/RegisterScreen'
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <Header></Header>
       <main className='py-3'>
         <Container>
-          <Route path='/login' component={LoginScreen} exact />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
