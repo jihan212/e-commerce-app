e7759b42df046495a4fa174aac893353527a2ee9
snapper <zihanjashim2121@gmail.comm>
2021-08-16 22:17:54 +0600

    Shipping Screen Added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screen/CartScreen'
 import LoginScreen from './screen/LoginScreen'
 import RegisterScreen from './screen/RegisterScreen'
 import ProfileScreen from './screen/ProfileScreen'
+import ShippingScreen from './screen/ShippingScreen'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Header></Header>
       <main className='py-3'>
         <Container>
+          <Route path='/shipping' component={ShippingScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
