06dce0c16a5f1c89f239dd722d161caa109edd67
snapper <zihanjashim2121@gmail.comm>
2021-08-18 23:38:54 +0600

    Order Screen Added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screen/ProfileScreen'
 import ShippingScreen from './screen/ShippingScreen'
 import PaymentScreen from './screen/PaymentScreen'
 import PlaceOrderScreen from './screen/PlaceOrderScreen'
+import OrderScreen from './screen/OrderScreen'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
           <Route path='/payment' component={PaymentScreen} />
           <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/login' component={LoginScreen} />
