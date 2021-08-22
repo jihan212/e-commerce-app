56976a9cad0448b212ed2d6c58fc1be97ecf7776
snapper <zihanjashim2121@gmail.comm>
2021-08-22 21:20:33 +0600

    Admin User Delete Added

--- front-end/src/reducers/userReducers.js
+++ front-end/src/reducers/userReducers.js
@@ -93,12 +93,12 @@ export const userListReducer = (state = { users: [] }, action) => {
   }
 }
 
-export const userDeleteReducer = (state = { users: [] }, action) => {
+export const userDeleteReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_DELETE_REQUEST:
       return { loading: true }
     case USER_DELETE_SUCCESS:
-      return { loading: false, users: action.payload }
+      return { loading: false, success: true }
     case USER_DELETE_FAIL:
       return { loading: false, error: action.payload }
     default:
