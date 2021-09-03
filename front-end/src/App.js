07ff976b8efe5ff735f060c87d2613852a718621
snapper <zihanjashim2121@gmail.comm>
2021-09-03 20:58:55 +0600

    Get User By ID & Update User Endpoints & User Edit Screen & Get User Details

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from './screen/PaymentScreen'
 import PlaceOrderScreen from './screen/PlaceOrderScreen'
 import OrderScreen from './screen/OrderScreen'
 import UserListScreen from './screen/UserListScreen'
+import UserEditScreen from './screen/UserEditScreen'
 
 const App = () => {
   return (
@@ -31,6 +32,7 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
